545a5835e0b6df04a07e127059e01a95e0d38b30
JoshiCodes <joshua.bickei@icloud.com>
2022-04-17 23:29:21 +0200

    Made some changes in the README.md

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import Wave from "./components/wave";
+import ProjectsContainer from "./components/projects";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+          <div className={"waveContainer"}>
+              <section id={"home"}>
+                  <div className={"pt-3 lg:pt-12"}>
+                      <h1 className={"text-5xl md:text-6xl lg:text-7xl font-bold"}>Willkommen</h1>
+                      <h4 className={"hidden md:block px-28 pt-6 lg:pt-12 text-xl md:text-2xl lg:text-3xl text-gray-200"}>Hey, ich bin Joshua und das ist meine Portfolio-Seite.<br/> Hier können Sie meine letzten Projekte sowie etwas über mich finden.</h4>
+                      <h4 className={"block md:hidden px-12 pt-6 lg:pt-12 text-xl md:text-2xl lg:text-3xl text-gray-200"}>Hey, ich bin Joshua!<br/>Hier finden Sie meine Projekte sowie etwas über mich!</h4>
+                  </div>
+              </section>
+              <div className={"pt-16 lg:pt-40"}>
+                  <Wave />
+              </div>
+          </div>
+
+          <section id={"about"} className={"mx-3 md:mx-8 pt-8 md:pt-12"}>
+              <div className={"pt-3 text-center"}>
+                  <h2 className={"text-4xl md:text-6xl font-semibold pb-6"}>Über mich</h2>
+
+              </div>
+          </section>
+
+          <section id={"projects"} className={"mx-3 md:mx-8 pt-8 md:pt-12"}>
+              <div className={"pt-3 text-center"}>
+                  <h2 className={"text-4xl md:text-6xl font-semibold pb-6"}>Meine Projekte</h2>
+                  <ProjectsContainer />
+              </div>
+          </section>
+
+      </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import reportWebVitals from './reportWebVitals';
+
+import App from './App';
+import Contact from './pages/contact';
+import NotFound from './pages/errors';
+
+import Navbar from './components/navbar';
+
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
+import WelcomePopup from "./components/popup";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <WelcomePopup />
+      <BrowserRouter>
+          <Routes>
+              <Route path={"/*"} element={<Navbar />} />
+          </Routes>
+          <Routes>
+              <Route path={"/"} element={<App />} />
+              <Route path={"/contact"} element={<Contact />} />
+              <Route path={"*"} element={<NotFound />} />
+          </Routes>
+      </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
